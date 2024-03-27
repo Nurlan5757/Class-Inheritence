92462e2b6165463ba9ea1bbc5d03259642385de4
unknown <tu4pmkvys@code.edu.az>
2024-03-28 01:22:57 +0400

    tapsiriq

---
+++ Class, Inheritence/Models/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class__Inheritence.Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Nurlan = new Student("Nurlan", "Abbasov", "AB106", 100);
+
+            Nurlan.Getİnfo();
+            Nurlan.GetDiplomDegree();
+            Nurlan.CheckGraduation();
+
+
+        }
+    }
+}
---
+++ Class, Inheritence/Models/Student.cs
@@ -0,0 +1,67 @@
+﻿        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text;  
+        using System.Threading.Tasks;
+
+namespace Class__Inheritence.Student
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public Boolean isGraduated=true;
+
+        
+        public Student(string name, string surname, string group, int point) 
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+        }
+        
+        public Student(Boolean isqraduated)  
+        {
+            isGraduated = isqraduated; 
+        }
+
+        public void Getİnfo() 
+        {
+            Console.WriteLine($"{Group}-{Name} {Surname} {Point} ");
+        }
+        public void CheckGraduation() 
+        {
+            if (Point > 65)
+            {
+                Console.WriteLine("bu telebe mezun olmayib");
+            }
+            else
+            {
+                Console.WriteLine("bu telebe mezun olub");
+            }
+        }
+        public void GetDiplomDegree()   
+        {
+            if(Point < 65)
+            {
+                Console.WriteLine("yoxdur");
+            }
+            else if (Point>65 && Point<80)
+            {
+                Console.WriteLine("adi");
+            }
+            else if (Point>80 && Point<90)
+            {
+                Console.WriteLine("şeref");
+            }
+            else
+            {
+                Console.WriteLine("yuksek şeref");
+            }
+        }
+    }
+}
